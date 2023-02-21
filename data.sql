d4be113b72a384a24859d38c37d9d465accb2346
Alelign Ayana <alelignayana@gmail.com>
2023-02-21 11:12:06 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg) values('Agumon','3-Feb-2020',0,true,10.23);
+insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg) values('Gabumon','15-Nov-2018',2,true,8);
+insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg) values('Pikachu','7-Jan-2021',1,false,15.04);
+insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg) values('Devimon','12-May-2017',5,true,11);
+
