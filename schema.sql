f52586052a48c2e31744bf98f595cf54499ac4f0
Alelign Ayana <alelignayana@gmail.com>
2023-02-22 15:31:29 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -12,6 +12,42 @@ CREATE TABLE animals (
 
  alter table animals
  add column species varchar(250);
+ 
+ create table owners (
+ id int GENERATED ALWAYS AS IDENTITY,
+ full_name varchar(250),
+ age int,
+ primary key(id)
+);
+
+create table species (
+ id int GENERATED ALWAYS AS IDENTITY,
+ name varchar(250),
+ primary key (id)
+);
+
+alter table animals
+drop column species;
+select * from animals;
+
+alter table animals 
+add column species_id int;
+
+alter table animals
+add foreign key (species_id) references species(id); 
+
+
+alter table animals
+add column owner_id int;
+
+alter table animals 
+add foreign key (owner_id) references owners(id);
+
+select * from animals;
+
+
+
+
 
 
 
