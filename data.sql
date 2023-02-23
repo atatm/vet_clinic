2010e5c969b9cf1ee7cb99cb295666a92d98e2a8
Alelign Ayana <alelignayana@gmail.com>
2023-02-23 17:20:54 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -52,7 +52,22 @@ SET owner_id = (
     )
 );
 
-select * from animals;
+-- Insert the following data for vets:
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher','45','2000-04-23'),
+('Maisy Smith','26','2019-01-17'),('Stephanie Mendez','64','1981-05-04'),
+('Jack Harkness','38','2008-06-08');
+
+-- Insert the following data for specialties:
+INSERT INTO specializations (vet_id, species_id) VALUES 
+((SELECT id FROM vets WHERE name = 'William Tatcher'),
+ (SELECT id FROM species WHERE name = 'Pokemon')),
+((SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+ (SELECT id FROM species WHERE name = 'Pokemon')),
+((SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+ (SELECT id FROM species WHERE name = 'Digimon')),
+((SELECT id FROM vets WHERE name = 'Jack Harkness'),
+ (SELECT id FROM species WHERE name = 'Digimon'));
+
 
   
    INSERT INTO visits (animal_id, vet_id, visit_date)
